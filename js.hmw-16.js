8cc5cff907fe26dd9306db0b0851badb974c9c76
z-chubinidze <120126922+z-chubinidze@users.noreply.github.com>
2023-06-03 11:47:41 +0400

    Add files via upload

---
+++ js.hmw-16.js
@@ -0,0 +1,11 @@
+ 
+
+const masivi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let sum = 0, i = 1;
+
+while(i <= masivi.length) {
+    sum += i;
+    i++;
+}
+document.write('მასივის რიცხვთა ჯამი არის:' + " " + sum);
